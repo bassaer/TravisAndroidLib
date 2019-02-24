67ef7aefc599c9805c46f2576521226017b6fe0a
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-02-24 21:32:04 +0900

    add skip option

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+cur_ver=$(git describe --tags --abbrev=0)
+next_ver=$(scripts/changelog.sh -v)
+if [ $cur_ver = $next_ver ]; then
+    echo 'skip deploy'
+    exit 0
+fi
+
 # AAR build
 ./gradlew travismylib:assembleRelease
 
